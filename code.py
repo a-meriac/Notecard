6a54ea648946c152c23ade740e5be99c854736d1
Andre Meriac <andre@meriac.com>
2026-03-25 11:12:35 +0400

    Added button presses

--- code.py
+++ code.py
@@ -2,8 +2,19 @@ import usb_midi
 import adafruit_midi
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
+import time
+import board
+import digitalio
 
 midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
 
+button = digitalio.DigitalInOut(board.GP15)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
 while True:
-    midi.send(NoteOn(60, 120))  # Note C4, velocity 120
+    if not button.value:
+        midi.send(NoteOn(60, 120))
+        while not button.value:
+            pass
+        midi.send(NoteOff(60, 120))
