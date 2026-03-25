43d9bd49cc74bfde7fcfa7f54eb043275389833d
Andre Meriac <andre@meriac.com>
2026-03-25 11:23:43 +0400

    Support for multiple notes

--- code.py
+++ code.py
@@ -8,13 +8,26 @@ import digitalio
 
 midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
 
-button = digitalio.DigitalInOut(board.GP15)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
+def make_button(pin):
+    button = digitalio.DigitalInOut(pin)
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
+    return button
+
+buttons = [
+    (make_button(board.GP15), 60), #C
+    (make_button(board.GP14), 62), #D
+    (make_button(board.GP13), 64), #E
+]
+
+held = [False] * len(buttons)
 
 while True:
-    if not button.value:
-        midi.send(NoteOn(60, 120))
-        while not button.value:
-            pass
-        midi.send(NoteOff(60, 120))
+    for i, (button,note) in enumerate(buttons):
+        pressed = not button.value
+        if pressed and not held[i]:
+            midi.send(NoteOn(note, 127))
+            held[i] = True
+        elif not pressed and held[i]:
+            midi.send(NoteOff(note, 0))
+            held[i] = False
